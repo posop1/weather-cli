465f01c08dddac8a6c75ed075de0a00b95462718
Artur Yakhin <artur.yakhin52@gmail.com>
2022-09-08 17:39:09 +0500

    Finally commit

--- weather.js
+++ weather.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 import { getArgs } from './helpers/args.js';
-import { getWeather } from './services/api.service.js';
-import { printHelp, printSuccess, printError } from './services/log.service.js';
-import { saveKeyValue, TOKEN_DICTIONARY } from './services/storage.service.js';
+import { getWeather, getIcon } from './services/api.service.js';
+import { printHelp, printSuccess, printError, printWeather } from './services/log.service.js';
+import { saveKeyValue, TOKEN_DICTIONARY, getKeyValue } from './services/storage.service.js';
 
 const saveToken = async (token) => {
   if (!token.length) {
@@ -16,11 +16,24 @@ const saveToken = async (token) => {
     printError(e.message);
   }
 };
+const saveCity = async (city) => {
+  if (!city.length) {
+    printError('City not found');
+    return;
+  }
+  try {
+    await saveKeyValue(TOKEN_DICTIONARY.city, city);
+    printSuccess('City is saved');
+  } catch (e) {
+    printError(e.message);
+  }
+};
 
 const getForcast = async () => {
   try {
-    const weather = await getWeather(process.env.CITY);
-    console.log(weather);
+    const city = process.env.CITY ?? (await getKeyValue(TOKEN_DICTIONARY.city));
+    const weather = await getWeather(city);
+    printWeather(weather, getIcon(weather.weather[0].icon));
   } catch (e) {
     if (e?.response?.status == 404) {
       printError('City error');
@@ -35,15 +48,15 @@ const getForcast = async () => {
 const initCLI = () => {
   const args = getArgs(process.argv);
   if (args.h) {
-    printHelp();
+    return printHelp();
   }
   if (args.s) {
-    // Сохранить город
+    return saveCity(args.s);
   }
   if (args.t) {
     return saveToken(args.t);
   }
-  getForcast();
+  return getForcast();
 };
 
 initCLI();
